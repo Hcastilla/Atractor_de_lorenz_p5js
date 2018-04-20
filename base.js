3d1ced9443498ce62773b9d5a08b334759e16de9
Hernan Castilla <hcastillaq@gmail.com>
2018-04-20 13:12:32 -0500

    commit inicial

---
+++ base.js
@@ -0,0 +1,102 @@
+/**
+ * Inicializamos las variables
+ */
+let x = 1;
+let y = 1;
+let z = 1;
+/**
+ * Definimos las constantes
+ */
+const a = 10;
+const b = 28;
+const c = 8/3;
+
+/**
+* Arreglo para almacenar todos los puntos
+*/
+let puntos = [];
+
+/**
+ * configuración inicial cargada a p5js
+ */
+function setup()
+{
+  /**
+   * Creamos el canvas para 3d pantalla completa
+   */
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+/**
+ * funcion para dibujar cada x tiempo
+ */
+function draw()
+{
+  background('black');
+
+  translate(0, 200, 0);
+
+  /**
+   * Rotamos los ejes
+   */
+  rotateZ(1.38);
+  rotateX(-1.38);
+  rotateY(1.38);
+
+  /**
+   * Manipular con el mouse (click)
+   */
+  orbitControl();
+
+  /**
+   * escalamos el dibujo 
+   */
+  scale(8);
+
+  /**Añadimos puntos al arreglo */
+  añadirPuntos(2);
+
+
+  /**
+   * Damos la tonalidad Amarilla
+   */
+  fill(255, 215, 0);
+
+  /**
+   * Creamos la forma
+   */
+  beginShape()
+  puntos.forEach( p => {
+    vertex(p.x, p.y, p.z);
+    
+  });
+  endShape()
+
+}
+
+function añadirPuntos(num){
+  for( let i = 0; i < num; i++)
+  {
+    /**
+     * calculamos de las derivadas
+     */
+
+    let dt = 0.01;  
+    
+    let dx = (a * (y - x)) * dt;
+    let dy = (x * (b - z) - y) * dt;
+    let dz = (x * y - c * z)* dt;
+    
+    /**
+     * Actualizamos valores
+     */
+    x = x + dx;
+    y = y + dy;
+    z = z + dz;
+
+    /**
+     * Creamos un punto  y lo añadimos al arreglo
+     */
+    puntos.push(createVector(x, y, z));
+  }
+}
